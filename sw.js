24c4587df05972b0149189e89463adbc52bf7efa
dairocanas <dairocanhasmanrique@gmail.com>
2025-08-08 18:41:27 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v2025-08-08-4';
+const CACHE_VERSION = 'v2025-08-08-8';
 const CACHE_NAME = 'kairo-cache-' + CACHE_VERSION;
 const ASSETS = ['./','./index.html','./manifest.json','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
